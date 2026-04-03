9694fc391ff10b7aeb0feb2d6ec2324b8f436565
harjasdee-singh <harjasdeep-singh@myseneca.ca>
2026-04-03 18:04:16 -0400

    Update the tips.js

--- public/js/tips.js
+++ public/js/tips.js
@@ -69,7 +69,7 @@ export function initTips({ tips }) {
 
     async function checkTips() {
         try {
-            const res = await fetch(`${BASE_URL}/api/user/tips`, {
+            const res = await fetch(`${BASE_URL}/api/tips`, {
                 headers: { Authorization: "jwt " + token },
             });
 
@@ -103,7 +103,7 @@ export function initTips({ tips }) {
 
     disableBtn.onclick = async () => {
         try {
-            await fetch(`${BASE_URL}/api/user/tips`, {
+            await fetch(`${BASE_URL}/api/tips`, {
                 method: "PATCH",
                 headers: {
                     "Content-Type": "application/json",
